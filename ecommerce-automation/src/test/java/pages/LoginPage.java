5b177ff43fb86d4b507e40dd683a07db87b20b78
Jaiprakash Bisht <jaibisht5426@gmail.com>
2026-06-09 22:16:07 +0530

    Day 35 Task completed

--- ecommerce-automation/src/test/java/pages/LoginPage.java
+++ ecommerce-automation/src/test/java/pages/LoginPage.java
@@ -2,19 +2,69 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPage
 {
     WebDriver driver;
+    WebDriverWait wait;
 
     public LoginPage(WebDriver driver)
+
     {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     private By registerLink = By.xpath("//span[contains(text(),'Register')]");
 
+    private By email = By.name("email");
+    private By password = By.name("password");
+    private By loginButton = By.xpath("//button[contains(text(),'Login')]");
+
+    public void enterEmail(String value)
+    {
+        driver.findElement(email).sendKeys(value);
+    }
+
+    public void enterPassword(String value)
+    {
+        driver.findElement(password).sendKeys(value);
+    }
+
+    public void clickLogin() throws InterruptedException {
+        driver.findElement(loginButton).click();
+
+//        Thread.sleep(3000);
+//        wait.until(ExpectedConditions.alertIsPresent());
+//
+//        String alertText = driver.switchTo().alert().getText();
+//
+//        System.out.println(alertText);
+//
+//        driver.switchTo().alert().accept();
+    }
+
+    public void login(String e, String p) throws InterruptedException {
+        enterEmail(e);
+        enterPassword(p);
+        clickLogin();
+
+        wait.until(ExpectedConditions.alertIsPresent());
+
+        String alertText = driver.switchTo().alert().getText();
+
+        System.out.println(alertText);
+
+        Thread.sleep(3000);
+
+        driver.switchTo().alert().accept();
+    }
     public void clickRegister()
+
     {
         driver.findElement(registerLink).click();
     }
--- ecommerce-automation/src/test/java/tests/LoginTest.java
+++ ecommerce-automation/src/test/java/tests/LoginTest.java
@@ -1,25 +1,49 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.LoginPage;
 import utils.BaseTest;
 
 public class LoginTest extends BaseTest
 {
     @Test
-    public void validLoginTest() throws InterruptedException {
-//        driver.findElement(By.name("email")).sendKeys("raghushyam@gmail.com");
-//        driver.findElement(By.name("password")).sendKeys("RaghuGovind@1234");
-//
-//        driver.findElement(By.tagName("button"))
-//                .click();
+    public void validLoginTest() throws InterruptedException
+    {
+        LoginPage loginPage =
+                new LoginPage(driver);
 
-//        Assert.assertTrue(
-//                driver.getCurrentUrl()
-//                        .contains("viewproducts")
-//        );
+        loginPage.login(
+                "raghushyam@gmail.com",
+                "RaghuGovin@1234"
+        );
+
+        Assert.assertTrue(
+                driver.getCurrentUrl()
+                        .contains("viewproducts"),
+                "User is not redirected to Products Page"
+        );
 
 //        Thread.sleep(10000);
     }
+
+//    @Test
+//    public void emptyFieldsValidationTest() throws InterruptedException {
+//
+//        LoginPage loginPage = new LoginPage(driver);
+//
+//        loginPage.clickLogin();
+//
+//        WebElement email =
+//                driver.findElement(By.name("email"));
+//
+//        String validationMessage =
+//                email.getAttribute("validationMessage");
+//
+//        Assert.assertFalse(
+//                validationMessage.isEmpty()
+//        );
+//    }
 }
