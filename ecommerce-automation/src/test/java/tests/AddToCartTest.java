9d5b2b9462bfea7d6b5ec4bc0bba71c6392d47fc
Jaiprakash Bisht <jaibisht5426@gmail.com>
2026-06-22 23:05:43 +0530

    Day 43 Task Completed

--- ecommerce-automation/src/test/java/tests/AddToCartTest.java
+++ ecommerce-automation/src/test/java/tests/AddToCartTest.java
@@ -14,66 +14,138 @@ import java.util.List;
 
 public class AddToCartTest extends BaseTest
 {
+//    @Test
+//    public void addToCartTest()
+//            throws InterruptedException {
+//
+//        LoginPage loginPage =
+//                new LoginPage(driver);
+//
+//        ProductPage productPage =
+//                new ProductPage(driver);
+//
+//        loginPage.login(
+//                "raghushyam@gmail.com",
+//                "RaghuGovind@1234"
+//        );
+//
+//        Thread.sleep(3000);
+//
+//        productPage.clickAddToCart();
+//
+//        Thread.sleep(2000);
+//
+//        Alert alert =
+//                driver.switchTo().alert();
+//
+//        String alertText =
+//                alert.getText();
+//
+//        Assert.assertEquals(
+//                alertText,
+//                "Added to cart ✅"
+//        );
+//
+//        alert.accept();
+//
+//        Thread.sleep(3000);
+//
+//        driver.findElement(
+//                By.xpath("//button[contains(text(),'Cart')]")
+//        ).click();
+//
+//        List<WebElement> cartProducts =
+//                driver.findElements(
+//                        By.cssSelector(".cart-items")
+//                );
+//
+//
+//        Assert.assertTrue(
+//                cartProducts.size() > 0,
+//                "Product not added to cart"
+//        );
+//
+//        Thread.sleep(3000);
+//
+//        driver.findElement(
+//                By.xpath("//button[contains(text(),'➕')]")
+//        ).click();
+//    }
+//
+//    @Test
+//    public void validateCartItemsTest() throws InterruptedException {
+//
+//        LoginPage loginPage =
+//                new LoginPage(driver);
+//
+//        ProductPage productPage =
+//                new ProductPage(driver);
+//
+//        loginPage.login(
+//                "raghushyam@gmail.com",
+//                "RaghuGovind@1234"
+//        );
+//
+//        Thread.sleep(3000);
+//
+//        productPage.clickAddToCart();
+//
+//        Thread.sleep(2000);
+//
+//        Alert alert =
+//                driver.switchTo().alert();
+//
+//        String alertText =
+//                alert.getText();
+//
+//        Assert.assertEquals(
+//                alertText,
+//                "Added to cart ✅"
+//        );
+//
+//        alert.accept();
+//
+//        Thread.sleep(3000);
+//
+//        driver.findElement(
+//                By.xpath("//button[contains(text(),'Cart')]")
+//        ).click();
+//
+//        Thread.sleep(3000);
+//
+//        List<WebElement> cartItems =
+//                driver.findElements(
+//                        By.cssSelector(".cart-card")
+//                );
+//
+//        System.out.println(
+//                "Total Cart Items: "
+//                        + cartItems.size()
+//        );
+//
+//        Assert.assertTrue(
+//                cartItems.size() > 0,
+//                "Cart is empty"
+//        );
+//
+//        Thread.sleep(2000);
+//
+//        JavascriptExecutor js =
+//                (JavascriptExecutor) driver;
+//
+//        js.executeScript(
+//                "window.scrollBy(0,500)"
+//        );
+//
+//        Thread.sleep(2000);
+//
+//        driver.findElement(
+//                By.xpath("(//button[contains(text(),'➕')])[2]")
+//        ).click();
+//    }
+
     @Test
-    public void addToCartTest()
-            throws InterruptedException {
-
-        LoginPage loginPage =
-                new LoginPage(driver);
-
-        ProductPage productPage =
-                new ProductPage(driver);
-
-        loginPage.login(
-                "raghushyam@gmail.com",
-                "RaghuGovind@1234"
-        );
-
-        Thread.sleep(3000);
-
-        productPage.clickAddToCart();
-
-        Thread.sleep(2000);
-
-        Alert alert =
-                driver.switchTo().alert();
-
-        String alertText =
-                alert.getText();
-
-        Assert.assertEquals(
-                alertText,
-                "Added to cart ✅"
-        );
-
-        alert.accept();
-
-        Thread.sleep(3000);
-
-        driver.findElement(
-                By.xpath("//button[contains(text(),'Cart')]")
-        ).click();
-
-        List<WebElement> cartProducts =
-                driver.findElements(
-                        By.cssSelector(".cart-items")
-                );
-
-
-        Assert.assertTrue(
-                cartProducts.size() > 0,
-                "Product not added to cart"
-        );
-
-        Thread.sleep(3000);
-
-        driver.findElement(
-                By.xpath("//button[contains(text(),'➕')]")
-        ).click();
-    }
-
-    @Test
-    public void validateCartItemsTest() throws InterruptedException {
+    public void validateCartItemsTestUpdateCart() throws InterruptedException {
 
         LoginPage loginPage =
                 new LoginPage(driver);
@@ -142,5 +214,91 @@ public class AddToCartTest extends BaseTest
         driver.findElement(
                 By.xpath("(//button[contains(text(),'➕')])[2]")
         ).click();
+
+        Thread.sleep(2000);
+
+        driver.findElement(
+                By.xpath("(//button[contains(text(),'➖')])[2]")
+        ).click();
+
+        Thread.sleep(3000);
+    }
+
+    @Test(priority = 1)
+    public void removeCartItemTest() throws InterruptedException {
+
+        LoginPage loginPage =
+                new LoginPage(driver);
+
+        ProductPage productPage =
+                new ProductPage(driver);
+
+        loginPage.login(
+                "raghushyam@gmail.com",
+                "RaghuGovind@1234"
+        );
+
+        Thread.sleep(3000);
+
+        productPage.clickAddToCart();
+
+        Thread.sleep(2000);
+
+        Alert alert =
+                driver.switchTo().alert();
+
+        String alertText =
+                alert.getText();
+
+        Assert.assertEquals(
+                alertText,
+                "Added to cart ✅"
+        );
+
+        alert.accept();
+
+        Thread.sleep(3000);
+
+        driver.findElement(
+                By.xpath("//button[contains(text(),'Cart')]")
+        ).click();
+
+        Thread.sleep(3000);
+
+        List<WebElement> cartItems =
+                driver.findElements(
+                        By.cssSelector(".cart-card")
+                );
+
+        System.out.println(
+                "Total Cart Items: "
+                        + cartItems.size()
+        );
+
+        Assert.assertTrue(
+                cartItems.size() > 0,
+                "Cart is empty"
+        );
+
+
+        driver.findElement(
+                By.xpath("(//button[contains(text(),'Remove ❌')])[1]")
+        ).click();
+
+        Thread.sleep(2000);
+
+        List<WebElement> itemsAfter =
+                driver.findElements(
+                        By.cssSelector(".cart-card")
+                );
+
+        int afterCount =
+                itemsAfter.size();
+
+        System.out.println(
+                "After Remove = "
+                        + afterCount
+        );
+
     }
 }
